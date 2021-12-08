0bc1e4f3b9c5888079346801ace093efd2b1f779
Charles Blake <charlechaud@gmail.com>
2021-12-08 14:18:17 -0500

    Very preliminary initial commit.

---
+++ spfun/prob.nim
@@ -0,0 +1,5 @@
+## Module for various special functions of classical probability theory (that
+## are inspecific to any particular probabilility density/distribution).
+
+proc hoeffding*[T](n: int; t: T): T = T(2) * exp(T(-2) * T(n) * t * t)
+  ## https://en.wikipedia.org/wiki/Hoeffding%27s_inequality
