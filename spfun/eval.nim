5dd5d014a3d601fde172f0074292bbf60b205e99
Charles Blake <charlechaud@gmail.com>
2021-12-13 10:10:11 -0500

    Document `lentz` better & simplify logic to more canonical; Also add `minIt` (based on Lentz-1990 work on Bessel functions).

--- spfun/eval.nim
+++ spfun/eval.nim
@@ -7,23 +7,24 @@ template lentz*(F: type,                # FP type
                 num, den: untyped,      # numerator, denominator coef series
                 f, n, est: untyped,     # var F: running val, it & estim REL.ERR
                 tol=1e-7,               # minimum convergence error tolerance
+                minIt=2,                # minimum number of coefs
                 maxIt=5000,             # maximum number of coefs
                 eps=1e-30,              # replacement for 0.0 in x/0.0 contexts
                 den0=den(0)) =          # if this is simpler than B(n) (eg. 0.0)
-  ## Continued fraction evaluator by modified Lentz method.  This is a template
-  ## in terms of templates `num(n)` ,`den(n)` since continued fraction series
-  ## have variadic mixed type (int/float) inputs.  Leaving all that type/param
-  ## count fixing to a parent proc/func seems an ok way to abstract over that.
-  ## NOTE: num(n) and den(n) are indexed starting from 1 as this is the common
-  ## convention for series coefficient formulae.
-  n = 1              # iteration number
+  ## Continued fraction evaluator by modified Lentz method.  I.e., evaluate
+  ##               num1   num2
+  ##   f = den0 + ------ ------ .. = den0 + num1/(den1 + num2/(den2 + ..))
+  ##              den1 + den2 +
+  ## This is a template in terms of templates `num(n)` ,`den(n)` since continued
+  ## fraction series have variadic mixed type (int/float) inputs.  Leaving all
+  ## that type/param count fixing to a parent proc/func seems an ok way to
+  ## abstract over that.  NOTE: num(0) is unused.
   var A,B,C,D,scl: F # num,den,running C=PN(n)/PN(n-1),D=PD(n)/PD(n-1),updateFac
-  C = den0
-  if (f = C; f < F(eps)):               # Handle 1st step specially
-    C = F(0.5) / F(eps)                 # Suppress num(2) for C; could be `inf`
-    D = F(1) / safeDivisor(den(1), F(eps)) 
-    f = num(1) * D
-    n = 2
+  f = den0
+  if f.abs < F(eps): f = eps # Could copySign; Shouldn't matter since it's eps
+  C = f
+  D = F(0)
+  n = 1              # iteration number
   while n < maxIt:
     A = num(n)
     B = den(n)
@@ -33,6 +34,6 @@ template lentz*(F: type,                # FP type
     scl = C * D
     f *= scl
     est = abs(scl - F(1))
-    if est < F(tol):
+    if est < F(tol) and n >= minIt:
       break
     inc n
