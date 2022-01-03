d7318d28e10637a7db7c84073339de64d91d8002
Charles Blake <charlechaud@gmail.com>
2022-01-03 07:54:49 -0500

    Add pvalue for correlation coef calculation.

--- spfun/studentT.nim
+++ spfun/studentT.nim
@@ -1,7 +1,14 @@
 ## Provide various func/sprocs related to the Student's T distribution
 
-import beta
+import beta, math
 
-func T_cdf(df, t: float): float =
+proc cdf*[F](df, t: F): F =
   ## CDF of the student's T statistic of `df` degrees of freedom
-  return betaI(abs(df / (df + t * t)), 0.5 * df, 0.5, err=1e-7)
+  return betaI(abs(df / (df + t * t)), F(0.5) * df, F(0.5), err=1e-7)
+
+proc corrP*[F](r: F; n: int): F =
+  ## P(Pearson linear corr coef > r|null hypothesis of linear independence).
+  if abs(r) >= 1 or n < 2:
+    F(0)
+  else:
+    studentT.cdf(F(n - 2), r * sqrt(F(n - 2) / ((F(1) - r) * (F(1) + r))))
