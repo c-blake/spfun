bc2e819b4145ea6d7f7d62c10f81a7c061e57557
Charles Blake <charlechaud@gmail.com>
2024-05-31 18:50:32 -0400

    Add Massart inequality based confidence bands.  Factor two very distinct visualization ideas into two procs with one driver.  In light of that, change `nCI:int` to `ci:float` now interpreted as dP spacing of CIs in `blur` mode and the width of the "CI tube" in `tube` mode.  Also add & document new enum for tube options (sides of tube pw, sim, diff). Also document `xlabel` in `help` which was missing before.

--- spfun/cdplot.nim
+++ spfun/cdplot.nim
@@ -1,10 +1,11 @@
 when not declared(stdin): import std/[syncio, formatfloat]
 import std/[strutils, strformat, algorithm]
-from spfun/binom     import initBinomP, est
-from cligen/osUt     import mkdirOpen
-from cligen/colorScl import rgb, hex
-type ConfBand = enum pointWise, simultaneous
-type Floats = seq[float] #^en.wikipedia.org/wiki/CDF-based_nonparametric_confidence_interval
+from spfun/binom import initBinomP, est
+from cligen/colorScl import rgb, hex # For details see: en.wikipedia.org/wiki/
+from cligen/osUt import mkdirOpen # CDF-based_nonparametric_confidence_interval
+type ConfBand* = enum pointWise, simultaneous, tube
+type TubeOpt* = enum pw="pointWise", sim="simultaneous", df="diff"
+type Fs = seq[float]; type Strs = seq[string]
 
 iterator edf*[T](ts: seq[T]): (T, int) =
   ## (x, CumCnt(x)) unnormalized EDF with ties of already sorted seq `ts`; Unix
@@ -19,19 +20,13 @@ iterator edf*[T](ts: seq[T]): (T, int) =
   if c > 0:                     # Could be empty
     yield (tLast, c)
 
-proc cdplot*(tp="/tmp/cd/", gplot="", nCI=20, band=pointWise, wvls: Floats= @[],
-             vals: Floats = @[], alphas: Floats = @[], xlabel="Sample Value",
-             inputs: seq[string]) =
-  ## Generate files & gnuplot script to render CDFs with confidence band blurs.
-  ## If `.len<inputs.len` the final value of `wvls`, `vals`, or `alphas` is
-  ## re-used for subsequent inputs, otherwise they match pair-wise.
-  let inputs = if inputs.len > 0: inputs else: @[""]
-  template setup(id, arg, default) =    # Ensure ok (wvls|vals|alphas)[i] ..
-    var id = arg                        #.. for each inputs[i].
-    if id.len == 0: id.add default
-    for i in 1 .. inputs.len - id.len: id.add id[^1]
-  setup wvls, wvls, 0.87; setup vals, vals, 1.0; setup alphas, alphas, 0.5
-  for p in inputs:
+proc massartBand(c, n: int; ci=0.95): (float, float) =
+  let eps = 2.0/(1.0 - ci)/(2.0*n.float)
+  (max(0.0, c.float/n.float - eps), min(1.0, c.float/n.float + eps))
+
+proc blur*(k=pw, ci=0.1; tp,gplot,xlabel:string; wvls,vals,alphas:Fs; ps:Strs) =
+  let nCI = int(0.5/ci - 0.5)   # + 1 gets added in divisor
+  for p in ps:
     var xs: seq[float]
     for f in lines(if p.len>0: p.open else: stdin): xs.add f.strip.parseFloat
     xs.sort; let n = xs.len             # Make, then emit EDF, C.Band files
@@ -44,7 +39,9 @@ proc cdplot*(tp="/tmp/cd/", gplot="", nCI=20, band=pointWise, wvls: Floats= @[],
       e.write f," ",c.float/n.float,"\n"
       for j in 1..nCI:
         let ci = j.float/float(nCI + 1)
-        let (lo, hi) = initBinomP(c, n).est(ci)
+        let (lo, hi) = if   k == pw : initBinomP(c, n).est(ci)
+                       elif k == sim: massartBand(c, n, ci)
+                       else: (c.float/n.float, c.float/n.float) # no range
         cifs[2*j-2].write f," ",lo,"\n"
         cifs[2*j-1].write f," ",hi,"\n"
     e.close; for cif in cifs: cif.close
@@ -52,11 +49,9 @@ proc cdplot*(tp="/tmp/cd/", gplot="", nCI=20, band=pointWise, wvls: Floats= @[],
   g.write &"""#!/usr/bin/gnuplot
 # set terminal png size 1920,1080 font "Helvetica,10"; set output "cbands.png"
 set key top left noautotitle    # EDFs go bot left->up right;Dot keys crowd plot
-set style data steps
-set xlabel "{xlabel}"
-set ylabel "Probability"
+set style data steps; set ylabel "Probability"; set xlabel "{xlabel}"
 plot """
-  for i, p in inputs:
+  for i, p in ps:
     for j in 1..nCI:
       let cCI = rgb(wvls[i], sat=1.0 - j.float/float(nCI + 1), val=vals[i]).hex
       let alph = if alphas[i] < 1.0: int(alphas[i]*256).toHex[^2..^1] else: ""
@@ -68,13 +63,34 @@ plot """
     g.write &",\\\n     '{tp}/{p}EDF'  lw 3 lc rgb '#{cEDF}' t '{lab}'"
   g.write "\n"; g.close
 
+proc tube*(k=pw, ci=0.95;tp,gplot,xlabel:string; wvls,vals,alphas:Fs; ps:Strs) =
+  discard
+
+proc cdplot*(band=pointWise,ci=0.02, tp="/tmp/cd/",gplot="",xlabel="Sample Val",
+  wvls:Fs= @[], vals:Fs= @[], alphas:Fs= @[], opt=df, inputs: Strs) =
+  ## Generate files & gnuplot script to render CDF as confidence band blur|tube.
+  ## If `.len < inputs.len` the final value of `wvls`, `vals`, or `alphas` is
+  ## re-used for subsequent inputs, otherwise they match pair-wise.
+  let inputs = if inputs.len > 0: inputs else: @[""]
+  template setup(id, arg, default) =    # Ensure ok (wvls|vals|alphas)[i] ..
+    var id = arg                        #.. for each inputs[i].
+    if id.len == 0: id.add default
+    for i in 1 .. inputs.len - id.len: id.add id[^1]
+  setup wvls, wvls, 0.87; setup vals, vals, 1.0; setup alphas, alphas, 0.5
+  case band
+  of pointWise   : blur pw,  ci, tp, gplot, xlabel, wvls, vals, alphas, inputs
+  of simultaneous: blur sim, ci, tp, gplot, xlabel, wvls, vals, alphas, inputs
+  of tube        : tube opt, ci, tp, gplot, xlabel, wvls, vals, alphas, inputs
+
 when isMainModule:
   import cligen; include cligen/mergeCfgEnv; dispatch cdplot, help={
     "inputs": "input paths or \"\" for stdin",
-    "tp"    : "temp path prefix for numbered CI levels",
+    "band"  : "bands: pointWise simultaneous tube",
+    "ci"    : "CI level | dP spacing for conf.bands",
+    "tp"    : "tmp path prefix for numbered CI levels",
     "gplot" : "gnuplot script or \"\" for stdout",
-    "nCI"   : "number CI levels for conf.band shading",
-    "band"  : "pointWise|simultaneous confidence bands",
+    "xlabel": "x-axis label; y is always probability",
     "wvls"  : "cligen/colorScl HSV-based wvlens; 0.6",
     "vals"  : "values (V) of HSV fame; 0.8",
-    "alphas": "alpha channel transparencies; 0.5"}
+    "alphas": "alpha channel transparencies; 0.5",
+    "opt"   : "tube opts: pointWise simultaneous diff"}
