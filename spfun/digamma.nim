04bf00561c7ff7cd28eafb8f429bfdb004911c58
Charles Blake <charlechaud@gmail.com>
2024-10-01 03:48:34 -0400

    Re-arrange comments to be (more) readable.

--- spfun/digamma.nim
+++ spfun/digamma.nim
@@ -1,9 +1,8 @@
 var psis = @[0.0, -0.5772156649015328606]   # psis[i] <- DiGammaψ[i]
 
-proc psi*(n: int): float =  # Just use ψ[i+1] - ψ[i] = 1/i recursion to..
-  ## Array-based DiGamma ψ(psi) function for integral arguments.
-  let n0 = psis.len     #.. lazily populate an array of cached answers.
-  if n0 < n + 2:
+proc psi*(n: int): float = ## Array-based DiGamma ψ(psi) for integral arguments.
+  let n0 = psis.len         # Just use ψ[i+1] - ψ[i] = 1/i recursion to..
+  if n0 < n + 2:            #.. lazily populate an array of cached answers.
     psis.setLen n + 2
     for i in n0 - 1 .. n:
       psis[i + 1] = psis[i] + 1/i.float
